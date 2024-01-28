98513429b6da879e15ff060aa16cdd36f4c95df8
Sting <bobadillasting@gmail.com>
2024-01-28 15:20:25 -0300

    refactor movie service

---
+++ src/service/movie_service.js
@@ -0,0 +1,62 @@
+import { API_KEY } from '../utils/consts'
+
+export const MovieService = {
+
+    getBy: getMovies,
+    get: getMovie
+}
+
+async function getMovies ({search}) {
+    if (search.trim() === '') return null
+
+    try {
+        const res = await fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${search}`)
+        const data = await res.json()
+        
+        const movies = data.Search?.map( movie => ({
+                id: movie.imdbID,
+                title: movie.Title,
+                type: movie.Type,
+                year: movie.Year,
+                poster: movie.Poster
+            }))
+
+        return movies
+
+    } catch(err) {
+        throw new Error('Error searching movies')
+    }
+}
+
+async function getMovie({params}) {
+
+    const {movieId} = params
+
+    if(movieId.trim() === '') return null
+
+    try {
+
+        const res = await fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&i=${movieId}&plot=full`)
+        const data = await res.json()
+
+        if(data.Response === 'False') {
+            throw new Error(data.Error)
+        }
+
+        const movie = {}
+
+        movie.id = data.imdbID
+        movie.title = data.Title
+        movie.description = data.Plot
+        movie.poster = data.Poster
+        movie.actors = data.Actors
+        movie.language = data.Language
+        movie.genre = data.Genre
+        movie.duration = data.Runtime
+
+        return movie
+
+    } catch(err) {
+        throw new Error('Error getting movie')
+    }
+}
