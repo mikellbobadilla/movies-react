a47e9562c59898fed7b8f955ad46a4957a66df97
Sting <bobadillasting@gmail.com>
2024-01-28 20:37:39 -0300

    remove loader react-router-dom

--- src/router.jsx
+++ src/router.jsx
@@ -1,7 +1,6 @@
 import { createBrowserRouter } from 'react-router-dom'
 import { Home } from './pages/Home'
 import { Movie } from './pages/Movie'
-import { MovieService } from './service/movie_service'
 
 export const router = createBrowserRouter([
     {
@@ -11,6 +10,5 @@ export const router = createBrowserRouter([
     {
         path: '/movies/:movieId',
         element: <Movie />,
-        loader: MovieService.get
     }
 ])
