ca745fd7c6fd3d7254eccb66c07a3b45f74913d0
Sting <bobadillasting@gmail.com>
2024-01-28 20:38:55 -0300

    use costom hook useMovie and useParams

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,15 +1,13 @@
-import { useLoaderData } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 import { MovieDetail } from '../components/MovieDetail'
-import { useEffect } from 'react'
+import { Spinner } from '../components/Spinner'
+import { useMovie } from '../hooks/useMovie'
 
 export function Movie() {
+    const { movieId } = useParams()
+    const { movie, loading, error } = useMovie({ movieId })
 
-    useEffect(() => {
-        document.documentElement.scrollTo(0, 0)
-    }, [])
-
-    const movie = useLoaderData()
-    return (
-        <MovieDetail movie={movie} />
-    )
-}
+    return loading
+        ? <Spinner size='size-16 mx-auto' position='flex justify-center items-center mt-10' />
+        : <MovieDetail movie={movie} error={error} />
+}   
